c718b0d18ded53919943c59100daf5bd1c376f3a
fidel castro <cfidel209@gmail.com>
2022-09-06 09:32:58 +0300

    Complete Assignment

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
+let title = document.getElementById('card-title')
+let image = document.getElementById("card-image")
+let likes = document.getElementById('like-count')
+let dislikes = document.getElementById('dislike-count')
+let list = document.getElementById('comments-list')
 
+fetch('http://Grub-hub/Grub-views')
+    .then(response => response.json())
+    .then(data => {
+        title.textContent = data.title
+        image.src = data.image
+        likes.textContent = data.likes
+        list.innerHTML = ''
+        data.comments.forEach(element => {
+            let li = document.createElement('li')
+            li.textContent = element.content
+            list.append(li)
+
+        });
+    })
+let button = document.getElementById('like-button')
+button.addEventListener('click', function () {
+    let numlikes = parseInt(likes.textContent)
+    likes.textContent = numlikes + 1
+
+
+
+let button = document.getElementById('dislike-button')
+button.addEventListener('click', function () {
+    let numdislikes = parseInt(likes.textContent)
+    likes.textContent = numdislikes + 1
+
+})
+})
+
+let form = document.getElementById('comment-form')
+form.addEventListener('submit', function (e) {
+    e.preventDefault()
+    let li = document.createElement('li')
+    li.textContent = e.target.comment.value
+    list.append(li)
+
+    form.reset ()
+})
